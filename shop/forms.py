254d057a444e269cc7b52bee5e79cc36e6089466
Amitabh Raj <83171896+Amitabhraj@users.noreply.github.com>
2021-09-12 09:02:08 +0530

    Update forms.py

--- shop/forms.py
+++ shop/forms.py
@@ -2,10 +2,10 @@ from django import forms
 from . models import *  
 
 class MyfileUploadForm(forms.Form):
-    file_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    file_category = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    file_subcategory = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    file_price = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    file_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':"Enter Product Name"}))
+    file_category = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':"Enter Product category"}))
+    file_subcategory = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':"Enter Product Subcategory"}))
+    file_price = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','placeholder':"Don't write '$' before price, Enter Just Price in Number"}))
     file_id = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control','readonly':'readonly'}))
     files_data = forms.FileField(widget=forms.FileInput(attrs={'class':'form-control'}))
 
