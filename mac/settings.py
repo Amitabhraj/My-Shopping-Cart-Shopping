f27aecd18aab04b859dab4419687480cfd2379ea
Amitabhraj <abhiraj1709w@gmail.com>
2021-12-26 21:19:50 +0530

    added url in mac/url

--- mac/settings.py
+++ mac/settings.py
@@ -208,7 +208,17 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 
-STATIC_ROOT = "/My-Shopping-Cart-Shopping/shop/static/"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'shop')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 
