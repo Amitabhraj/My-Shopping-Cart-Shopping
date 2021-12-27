bcc7d9c54169b33855915f7e505624c4721b8869
Amitabhraj <abhiraj1709w@gmail.com>
2021-12-27 20:57:40 +0530

    improved

--- mac/urls.py
+++ mac/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url
 from django.urls import path, include
 from django.conf import settings
 from . import views
@@ -24,8 +23,8 @@ from django.contrib.auth.views import LogoutView, LoginView
 from django.views.static import serve
 
 urlpatterns = [
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
