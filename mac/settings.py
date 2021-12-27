4f1c97c605ebaf3a87419566bd866b57b745a70a
Amitabhraj <abhiraj1709w@gmail.com>
2021-12-27 10:27:19 +0530

    more things changed

--- mac/settings.py
+++ mac/settings.py
@@ -212,7 +212,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'shop/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'shop')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
