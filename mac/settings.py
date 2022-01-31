896177daa63343b431524e2aac13ed4c677f718c
Amitabhraj <abhiraj1709w@gmail.com>
2022-01-31 19:23:16 +0530

    Code reverted To Previous Version

--- mac/settings.py
+++ mac/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&2vq6i5uf_^1yr@kdpasd&ks!6uth9+a6jqf$c%j&1_=5o4tr!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -143,14 +143,14 @@ STATIC_URL = '/shop/static/'
   
 
 
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop')]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'shop/static')
+# if DEBUG:
+#     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop')]
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'shop/static')
 
   
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
